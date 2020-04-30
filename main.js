8e70f4722a8f6f35cbe1a333be6f2ab8a89eef2a
tommgarciag <56352971+tommgarciag@users.noreply.github.com>
2020-04-30 15:18:58 -0300

    ordena codigo y crea funciones para mostrar los resultados de la tarea 2

--- main.js
+++ main.js
@@ -8,39 +8,6 @@ document.querySelector("#siguiente-paso").onclick = function(event) {
     event.preventDefault();
 };
 
-document.querySelector("#calcular").onclick = function(event){
-    const numeros = obtenerEdadesIntegrantes();
-    mostrarEdad('mayor', obtenerMayorNumero(numeros));
-    mostrarEdad('menor', obtenerMenorNumero(numeros));
-    mostrarEdad('promedio', obtenerPromedio(numeros));
-    mostrarResultados();
-
-    event.preventDefault();
-};
-
-document.querySelector("#agregar-salarios").onclick = function(event){
-    const $cantidad = document.querySelector("#cantidad-integrantes");
-    const cantidad = Number($cantidad.value);
-
-    
-    crearSalarios(cantidad);
-
-    event.preventDefault();
-    
-};
-
-
-document.querySelector("#resetear").onclick = resetear;
-
-function borrarIntegrantesAnteriores(){
-    const $integrantes = document.querySelectorAll(".integrante");
-    for(let i = 0 ; i < $integrantes.length ;i++){
-        $integrantes[i].remove();
-    }
-}
-
-
-
 
 function crearIntegrantes(cantidadIntegrantes){ 
     if (cantidadIntegrantes > 0){
@@ -76,8 +43,34 @@ function crearIntegrante(indice){
     $integrantes.appendChild($div);  
 }
 
+function borrarIntegrantesAnteriores(){
+    const $integrantes = document.querySelectorAll(".integrante");
+    for(let i = 0 ; i < $integrantes.length ;i++){
+        $integrantes[i].remove();
+    }
+}
+
+
+//------------------------------------------------------------------------------------
+
+document.querySelector("#agregar-salarios").onclick = function(event){
+    const $cantidad = document.querySelector("#cantidad-integrantes");
+    const cantidad = Number($cantidad.value);
+
+    
+    crearSalarios(cantidad);
+
+    event.preventDefault();
+    
+};
+
+
 function crearSalarios(cantidadIntegrantes){ 
-  
+    if (cantidadIntegrantes > 0){
+        
+        mostrarBotonCalcularSalarios();
+    }
+
     for(let i = 0 ; i < cantidadIntegrantes ;i++){
         
         crearInputIngresoSalarios(i);       
@@ -102,12 +95,17 @@ function crearInputIngresoSalarios(indice){
     $salarios.appendChild($divSalarios);
 }
 
+// ----------------------------------------------------------------------------------
+
+document.querySelector("#resetear").onclick = resetear;
 
 function resetear(){
     borrarIntegrantesAnteriores();
     ocultarBotonCalculo();       
     ocultarResultados();
 }
+
+//----------------------------------------------------------------------------------
 
 function mostrarBotonCalculo(){
     document.querySelector("#calcular").className = "";
@@ -121,6 +119,12 @@ function mostrarBotonAgregarSalarios(){
     document.querySelector("#agregar-salarios").className = "";
 }
 
+function mostrarBotonCalcularSalarios(){
+    document.querySelector("#calcular-salarios").className = "";
+}
+
+// ----------------------------------------------------------------------------------
+
 function mostrarResultados(){
     document.querySelector("#analisis").className = "";
 }
@@ -128,6 +132,24 @@ function mostrarResultados(){
 function ocultarResultados(){
     document.querySelector("#analisis").className = "oculto";
 }
+
+function mostrarResultadoSalario(){
+    document.querySelector("#resultados").className = "";
+}
+
+
+
+// --------------------------------------------------------------------------
+
+document.querySelector("#calcular").onclick = function(event){
+    const numeros = obtenerEdadesIntegrantes();
+    mostrarEdad('mayor', obtenerMayorNumero(numeros));
+    mostrarEdad('menor', obtenerMenorNumero(numeros));
+    mostrarEdad('promedio', obtenerPromedio(numeros));
+    mostrarResultados();
+
+    event.preventDefault();
+};
 
 function mostrarEdad(tipo,valor){
     document.querySelector(`#${tipo}-edad`).textContent = valor;
@@ -142,5 +164,38 @@ function obtenerEdadesIntegrantes(){
     }
     return edades;
 }
+
+//-----------------------------------------------------------------------------
+
+document.querySelector("#calcular-salarios").onclick = function(event){
+    const inputSalarios = obtenerSalarios();
+    
+    mostrarSalario('mayor-anual', obtenerMayorNumero(inputSalarios));
+    mostrarSalario('menor-anual', obtenerMenorNumero(inputSalarios));
+    mostrarSalario('promedio-anual', obtenerPromedio(inputSalarios));
+    mostrarSalario('promedio-mensual', obtenerPromedioMensual(inputSalarios));
+    mostrarResultadoSalario();
+
+    event.preventDefault();
+};
+
+function mostrarSalario(tipo,valor){
+    document.querySelector(`#${tipo}-salario`).textContent = valor;
+}
+
+function obtenerSalarios(){
+    const $salarioIntegrante = document.querySelectorAll(".salario-integrante input");
+    const salarioIntegrantes = [];
+
+    for(let i = 0; i < $salarioIntegrante.length; i++){
+        if($salarioIntegrante[i].value === "") {
+            continue;            
+        }
+        salarioIntegrantes.push(Number($salarioIntegrante[i].value));
+    }
+    return salarioIntegrantes;
+}
+
+
 
 
